eb38cf45cf599118cfac5163a25b7c1ca76e6d2b
tramalho <thiagofacul@gmail.com>
2024-09-08 21:07:50 -0300

    🚧 Create PlayerName component without CSS

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ import "./src/components/styles/generic/reset.css";
 import "./src/components/styles/elements/base.css";
 
 import BoardGame from "./src/Objects/BoardGame";
+import PlayerName from "./src/components/PlayerName";
+
 
 
 // boa pratica usar $ para variaveis que guardam elementos do DOM
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+$root.insertAdjacentHTML("beforeend", 
+`
+  ${PlayerName("Player1")}
+  ${PlayerName("Player2")}
+  ${BoardGame(6)}
+`
+);
 
 console.log($htmlBoardGame);
